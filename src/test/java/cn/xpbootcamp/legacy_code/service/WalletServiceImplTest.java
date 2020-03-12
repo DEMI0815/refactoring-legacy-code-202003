913b075c9fe41b118e0b7344bd4a839ca4f4e3aa
Yidi <yidi.zhao@thoughtworks.com>
2020-03-12 12:14:44 +0800

    add more test for move money

--- src/test/java/cn/xpbootcamp/legacy_code/service/WalletServiceImplTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/service/WalletServiceImplTest.java
@@ -14,16 +14,20 @@ public class WalletServiceImplTest {
     @Test
     public void should_return_id_when_buyer_have_enough_money() {
         User buyer = new User();
+        User seller = new User();
         buyer.setBalance(10);
+        seller.setBalance(1);
 
         UserRepository userRepository = mock(UserRepository.class);
         WalletServiceImpl walletService = new WalletServiceImpl();
 
         when(userRepository.find(1)).thenReturn(buyer);
-        when(userRepository.find(2)).thenReturn(new User());
+        when(userRepository.find(2)).thenReturn(seller);
         walletService.setUserRepository(userRepository);
         String walletId = walletService.moveMoney("123", 1, 2, 10);
 
+        assertEquals(0, buyer.getBalance(), 0.001);
+        assertEquals(11, seller.getBalance(), 0.001);
         assertTrue(walletId.endsWith("123"));
     }
 
